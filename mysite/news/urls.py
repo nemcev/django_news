cfc93322871a782945d819efd47584fe93561528
nemcev <alexnemtsev@mail.ru>
2019-05-26 23:36:10 +0300

    add Future Imperfect by HTML5 UP
    add Future Imperfect by HTML5 UP templates (single - not working static files), urls and views.

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path 
 from . import views
 
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('single/', views.single, name='single'),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'news/index.html')
+
+
+def single(request):
+    return render(request, 'news/single.html')
