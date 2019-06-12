7f84e166b1afa6ba535c5596cbd3a2127a92df0f
nemcev <alexnemtsev@mail.ru>
2019-06-12 18:18:18 +0300

    enable postgreSQL and little more
    enable postgreSQL data base, add a little refactoring, add logo, began to create the structure of the site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news',
+    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newssite',
+        'USER': 'postgres',
+        'PASSWORD': 'miro1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=200)
+    img = models.URLField(max_length=200)
+    text = models.TextField()
+    pub_date = models.DateField('Дата публикации')    
